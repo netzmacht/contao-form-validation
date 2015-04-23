c35a357f0ac3152a21c22c28dfed07962b2e6c21
David Molineus <david.molineus@netzmacht.de>
2015-04-23 11:32:09 +0200

    Add headers.

--- module/config/services.php
+++ module/config/services.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * @package    contao-form-validation
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
 global $container;
 
 $container['form-validation.integration'] = $container->share(
